c02ea0b2a6ae7eac7cd5f5c56e09a8cea08020f3
aalyabd <aaly.abedy@gmail.com>
2025-01-29 18:54:30 +0330

    get ready for secuond app in my site (blog app)

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "blog"
+
 urlpatterns=[
-    path('',views.blog_view, name = 'blogview'),
-    path('about-me/',views.about_me, name= 'aboutme'),
+    path('',views.blog_view, name = 'index'),
+    path('single',views.about_me, name= 'single'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def about_me(requests):
-    return render(requests,'aboutme.html')
+    return render(requests,'blog/blog-home.html')
 
 def blog_view(requests):
-    return render(requests,'blog.html')
+    return render(requests,'blog/blog-single.html')
 
 
